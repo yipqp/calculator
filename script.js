aa16a1d73a144f61887852faa34e1761bafb0de4
yipqp <rdyip892@gmail.com>
2022-12-29 15:56:09 -0800

    Change display with numbers, +/-, decimal, and AC

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const numbers = document.querySelectorAll(".numbers");
+const display = document.querySelector("#display");
+const clear = document.querySelector("#clear");
+
+numbers.forEach(number => {
+    number.addEventListener("click", updateDisplay)
+});
+
+function updateDisplay() { 
+    const number = this.id;
+    let value = display.textContent;
+    
+    if (value === "0") {
+        if (number === "-") return;
+        display.textContent = number;
+    } else if (value.length < 17) {
+        if (number === "." && value.includes(".")) return;
+        if (number === "-") {
+            value.includes("-") ? value = value.slice(1) : value = number + value;       
+        } else {
+           value += number; 
+        }
+        display.textContent = value;
+    } 
+}
+
+clear.addEventListener("click", e => display.textContent = 0);
+
+
 function add(a, b) {
     return a + b;
 }
