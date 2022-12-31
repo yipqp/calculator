bdb856ecf386100d31477c7610d7ed8fec738276
yipqp <rdyip892@gmail.com>
2022-12-30 18:26:37 -0800

    Implement basic calculator functions

--- script.js
+++ script.js
@@ -1,82 +1,125 @@
-const numbers = document.querySelectorAll(".numbers");
+const mainDisplay = document.querySelector("#bottom-display");
+const topDisplay = document.querySelector("#top-display");
+const numbers = document.querySelectorAll(".numbers"); // includes numbers, +/-, decimal
 const operations = document.querySelectorAll(".operations");
-const display = document.querySelector("#bottom-display");
+const divideButton = document.querySelector("#divide");
+const multiplyButton = document.querySelector("#multiply");
+const subtractButton = document.querySelector("#subtract");
+const addButton = document.querySelector("#add");
+const equalButton = document.querySelector("#equals");
 const clear = document.querySelector("#clear");
-
-let prevValue;
-let nextValue;
-let operator;
+let prevValue = "0";
+let currentValue = "0";
+let operationSymbol = "";
+let currentOperator; // function
 let isOperating = false;
-let storingNextValue = false;
 
-numbers.forEach(number => {
-    number.addEventListener("click", updateDisplay);
+numbers.forEach(button => {
+    button.addEventListener("click", setCurrentValue);
 });
 
-function updateDisplay() { 
+function setCurrentValue() { 
     const pressed = this.id;
-    let value = display.textContent;
 
-    if (value === "0") {
+    if (currentValue === "0") {
         if (pressed === "-") return;
-        else if (pressed === ".") value += pressed;
-        else value = pressed;
-    } else if (value.length < 17) {
-        if (pressed === "." && value.includes(".")) return;
+        else if (pressed === ".") currentValue += pressed; 
+        else currentValue = pressed;
+    } else if (currentValue.length < 17) {
+        if (pressed === "." && currentValue.includes(".")) return;
         if (pressed === "-") {
-            value.includes("-") ? value = value.slice(1) : value = pressed + value;       
+            currentValue.includes("-") ? currentValue = currentValue.slice(1) : currentValue = pressed + currentValue;       
         } else {
-           value += pressed; 
+           currentValue += pressed; 
         }
     }
-    
-    if (isOperating) {
 
+    updateMainDisplay();
+}
+
+function updateMainDisplay() {
+    mainDisplay.textContent = currentValue;
+}
+
+function updateTopDisplay() {
+    topDisplayMessage = `${prevValue} ${operationSymbol} `;
+    if (currentValue !== "0") {
+        topDisplayMessage += `${currentValue} =`; 
     }
-
-    display.textContent = value;
+    topDisplay.textContent = topDisplayMessage;
 }
 
 clear.addEventListener("click", allClear);
 
 function allClear() {
-    display.textContent = 0;
+    mainDisplay.textContent = "0";
+    topDisplay.textContent = "";
+    currentValue = "0";
+    prevValue = "";
     isOperating = false;
-    storingNextValue = false;
-
+    currentOperator = undefined;
 }
 
 operations.forEach(operator => {
-    operator.addEventListener("click", storeValues);
+    operator.addEventListener("click", updateValues);
 });
 
-function storeValues() {
-    prevValue = display.textContent;
-    operator = this.id;
-    storingNextValue = true;
-
+function updateValues() {
+    operationSymbol = this.textContent;
     if (isOperating) {
-
+        calculate();
+    } else {   
+        prevValue = currentValue;     
+        updateMainDisplay();
+        currentValue = "0";
+        isOperating = true;        
     }
-    isOperating = true;
+    updateTopDisplay();
 }
 
-function add(a, b) {
-    return a + b;
+const add = function (a, b) {
+    return +a + +b;
 }
 
-function subtract(a, b) {
+addButton.addEventListener("click", () => storeOperator(add));
+
+const subtract = function (a, b) {
     return a - b;
 } 
 
-function multiply(a, b) {
+subtractButton.addEventListener("click", () => storeOperator(subtract));
+
+const multiply = function (a, b) {
     return a * b;
 }
 
-function divide(a, b) {
+multiplyButton.addEventListener("click", () => storeOperator(multiply));
+
+const divide = function (a, b) {
     return a / b;
 }
 
+divideButton.addEventListener("click", () => storeOperator(divide));
+
 function operate(operator, a, b) {
     return operator(a, b);
+}
+
+function storeOperator(operator) {
+    currentOperator = operator;
+} 
+
+equalButton.addEventListener("click", calculate);
+
+function calculate() {
+    updateTopDisplay(); 
+    if (currentOperator !== undefined) {
+        currentValue = operate(currentOperator, prevValue, currentValue).toString();
+        if (currentValue.length > 17) { // no long decimals
+            currentValue = currentValue.slice(0, 17);
+        }
+    } 
+    updateMainDisplay();
+    prevValue = "0";
+    isOperating = false;
 }
